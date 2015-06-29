2c39c544c7a4d2b16f75f0e52c70fafe2878c79c
klauspost <klauspost@gmail.com>
2015-06-29 14:25:28 +0200

    Add memory db with bulk for testing.

--- drivers/testdb/memdb.go
+++ drivers/testdb/memdb.go
@@ -17,3 +17,28 @@ func (m MemDB) Has(s string) (bool, error) {
 	_, ok := m[s]
 	return ok, nil
 }
+
+type MemDBBulk map[string]struct{}
+
+func NewMemDBBulk() *MemDBBulk {
+	m := MemDBBulk(make(map[string]struct{}))
+	return &m
+}
+
+func (m *MemDBBulk) AddMultiple(s []string) error {
+	db := *m
+	for _, p := range s {
+		db[p] = struct{}{}
+	}
+	return nil
+}
+func (m *MemDBBulk) Add(s string) error {
+	db := *m
+	db[s] = struct{}{}
+	return nil
+}
+
+func (m MemDBBulk) Has(s string) (bool, error) {
+	_, ok := m[s]
+	return ok, nil
+}
