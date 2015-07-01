27b68522d766fe2be454a9174c5156fcaeda9033
klauspost <klauspost@gmail.com>
2015-07-01 14:00:40 +0200

    Add Bolt database driver.

---
+++ drivers/boltpw/boltdb.go
@@ -0,0 +1,60 @@
+package pwbolt
+
+import (
+	"github.com/boltdb/bolt"
+)
+
+type BoltDB struct {
+	DB     *bolt.DB
+	Bucket []byte
+}
+
+func New(db *bolt.DB, bucket string) (*BoltDB, error) {
+	b := &BoltDB{DB: db, Bucket: []byte(bucket)}
+
+	// Execute several commands within a write transaction.
+	err := db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists([]byte(b.Bucket))
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+	return b, nil
+
+}
+
+func (b BoltDB) Has(s string) (bool, error) {
+	var res bool
+	err := b.DB.View(func(tx *bolt.Tx) error {
+		v := tx.Bucket(b.Bucket).Get([]byte(s))
+		res = v != nil
+		return nil
+	})
+	return res, err
+}
+
+func (b BoltDB) Add(s string) error {
+	return b.DB.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket(b.Bucket)
+		b.Put([]byte(s), []byte{})
+		return nil
+	})
+}
+
+func (b BoltDB) AddMultiple(s []string) error {
+	return b.DB.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket(b.Bucket)
+		for _, key := range s {
+			err := b.Put([]byte(key), []byte{})
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+}
---
+++ drivers/boltpw/boltdb_test.go
@@ -0,0 +1,37 @@
+package boltpw
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/boltdb/bolt"
+	"github.com/klauspost/password/drivers"
+)
+
+// tempfile returns a temporary file path.
+func tempfile() string {
+	f, _ := ioutil.TempFile("", "bolt-")
+	f.Close()
+	os.Remove(f.Name())
+	return f.Name()
+}
+
+// Test a bolt database
+func TestBolt(t *testing.T) {
+	db, err := bolt.Open(tempfile(), 0666, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(db.Path())
+	defer db.Close()
+
+	bolt, err := New(db, "commonpwd")
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = drivers.TestDriver(bolt)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
